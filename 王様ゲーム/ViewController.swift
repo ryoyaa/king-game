dc4ef2e1c7ddfbd46ac1f0b9813b98118bda9973
ishikawa ryoya <ryoya76lima@gmail.com>
2019-05-09 23:49:32 +0900

    Initial Commit

---
+++ 王様ゲーム/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  王様ゲーム
+//
+//  Created by 石川諒弥 on 2019/05/09.
+//  Copyright © 2019 石川諒弥. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 王様ゲームTests/______Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ______Tests.swift
+//  王様ゲームTests
+//
+//  Created by 石川諒弥 on 2019/05/09.
+//  Copyright © 2019 石川諒弥. All rights reserved.
+//
+
+import XCTest
+@testable import ______
+
+class ______Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
